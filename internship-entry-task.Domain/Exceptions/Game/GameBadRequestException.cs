6bc870c61f182e328f803a1da5e8e7cea6c1c8b8
Kosty <kosty.lygin.58@mail.ru>
2025-07-12 15:56:20 +0300

    Добавления исключений

---
+++ internship-entry-task.Domain/Exceptions/Game/GameBadRequestException.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using internship_entry_task.Domain.Exceptions.Shared;
+
+namespace internship_entry_task.Domain.Exceptions.Game;
+
+public class GameBadRequestException(string message) : BadRequestException(message)
+{
+    public static GameBadRequestException GameAlreadyFinished()
+    {
+        return new GameBadRequestException("Game already finished");
+    }
+
+    public static GameBadRequestException CellAlreadyOccupied(int row, int col)
+    {
+        return new GameBadRequestException($"Cell {row};{col} already occupied");
+    }
+}
