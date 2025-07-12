23f19ff157ee4870d08c58323595766145a0965a
Kosty <kosty.lygin.58@mail.ru>
2025-07-12 15:55:52 +0300

    Настройка Program.cs

--- internship-entry-task.Api/Program.cs
+++ internship-entry-task.Api/Program.cs
@@ -1,2 +1,46 @@
-var builder = WebApplication.CreateBuilder(args);
+using internship_entry_task.Api.Extensions;
+using internship_entry_task.Api.Middleware;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using HealthChecks.UI.Client;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(connectionString: configuration["GameDataBase:ConnectionString"],
+        name:"postgresql",
+        timeout: TimeSpan.FromSeconds(5),
+        tags: ["db", "sql", "postgres"]);
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDapper();
+builder.Services.MigrateDatabase(configuration);
+
+builder.Services.AddInfrastructure();
+builder.Services.AddApplication();
+builder.Services.AddSwagger();
+builder.Services.AddTransient<ExceptionHandlingMiddleware>();
+
+var app = builder.Build();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.MapControllers();
+
+app.Run();
+
+public class ProgramPlaceholder { }
