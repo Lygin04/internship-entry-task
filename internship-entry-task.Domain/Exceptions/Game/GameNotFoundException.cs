7a86c1a7379f68b182152a7b25a2f4e6a63f7e76
Kosty <kosty.lygin.58@mail.ru>
2025-07-12 15:56:20 +0300

    Добавления исключений

---
+++ internship-entry-task.Domain/Exceptions/Game/GameNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using internship_entry_task.Domain.Exceptions.Shared;
+
+namespace internship_entry_task.Domain.Exceptions.Game;
+
+/// <summary>
+/// Исключение, указывающее на то, что файл не был найден.
+/// </summary>
+public class GameNotFoundException(string message) : NotFoundException(message)
+{
+    public static GameNotFoundException WithSuchId(Guid id)
+    {
+        return new GameNotFoundException($"Game with id {id} has not been found");
+    }
+}
